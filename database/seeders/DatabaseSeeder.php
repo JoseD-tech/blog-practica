29fd6b7ee04f0a6d06f8ec565e740cc108236a4c
jose muñoz <jgmting@gmail.com>
2023-05-03 16:49:27 -0400

    factorys

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Comment;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +18,10 @@ class DatabaseSeeder extends Seeder
     {
         //llamar al seeder de usuarios
         $this->call(UserSeeder::class);
+
+        // factories
+        Category::factory(8)->create();
+        Article::factory(20)->create();
+        Comment::factory(20)->create();
     }
 }
